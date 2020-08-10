cf2d775e10f494bab42448a6b4ea0579e3cf6c1f
Nathan <nathanrjulian@gmail.com>
2020-08-10 19:26:00 +0000

    set up sprite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # import packages
 import pygame
+import random
 from pygame.locals import *
 
 pygame.init()
@@ -13,10 +14,24 @@ background_color = (120, 255, 172)
 #clock
 clock = pygame.time.Clock()
 
+#sprite
+sprite_image = pygame.image.load("replit.png")
+sprite_rect = sprite_image.get_rect()
+sprite_image = pygame.transform.scale(sprite_image, (int(0.1*sprite_rect.width), int (0.1*sprite_rect.height)))
+sprite_rect = sprite_image.get_rect()
+sprite_rect.center = (width // 2, height // 2)
+
+speed = pygame.math.Vector2(0, 10)
+rotation = random.randint(0, 360)
+speed.rotate_ip(rotation)
+
+
 def main():
 	while True:
 		clock.tick(60)
+		sprite_rect.move_ip(speed)
 		screen.fill(background_color)
+		screen.blit(sprite_image, sprite_rect)
 		pygame.display.flip()
 
 main()
